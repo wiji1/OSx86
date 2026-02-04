58672afb691e1756e1aeeb806b7b12ffc6ff0923
wiji <wijigaming.yt@gmail.com>
2026-02-04 15:34:43 -0800

    linker

--- riscv/src/main.rs
+++ riscv/src/main.rs
@@ -4,10 +4,16 @@
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    unsafe {
+        let uart: *mut u8 = 0x10010000 as *mut u8;
+        for &b in b"hello\n" {
+            uart.write_volatile(b);
+        }
+    }
     loop {}
 }
 
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
-    loop {}
+    panic(info)
 }
