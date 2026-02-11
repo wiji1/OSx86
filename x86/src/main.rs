1cc92ce539a13527f496a5df4b5d04184a21ab99
wiji <wijigaming.yt@gmail.com>
2026-02-11 14:03:43 -0800

    hello world

--- x86/src/main.rs
+++ x86/src/main.rs
@@ -2,8 +2,21 @@
 #![no_main]
 #![allow(unconditional_recursion)]
 
+const VGA_BUFFER: *mut u16 = 0xb8000 as *mut u16;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let message = b"Hello world! This is a cool program that has rainbow text. I wonder how long it can go.";
+
+    for (i, &byte) in message.iter().enumerate() {
+        let color_ord: u8 = ((i % 15) + 1) as u8;
+        let buffer: [u8; 2] = [byte, color_ord];
+
+        unsafe {
+            VGA_BUFFER.add(i).write(u16::from_le_bytes(buffer));
+        }
+    }
+
     loop {}
 }
 
