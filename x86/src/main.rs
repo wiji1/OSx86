7cf393b97fddd148061bfb09f5c78b637a5718a5
wiji <wijigaming.yt@gmail.com>
2026-02-18 14:32:01 -0800

    x86 vga

--- x86/src/main.rs
+++ x86/src/main.rs
@@ -2,25 +2,17 @@
 #![no_main]
 #![allow(unconditional_recursion)]
 
-const VGA_BUFFER: *mut u16 = 0xb8000 as *mut u16;
+mod vga;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    let message = b"Hello world! This is a cool program that has rainbow text. I wonder how long it can go.";
-
-    for (i, &byte) in message.iter().enumerate() {
-        let color_ord: u8 = ((i % 15) + 1) as u8;
-        let buffer: [u8; 2] = [byte, color_ord];
-
-        unsafe {
-            VGA_BUFFER.add(i).write(u16::from_le_bytes(buffer));
-        }
-    }
-
+pub fn _start() -> ! {
+    println!("Hello, world!\nHi\nNew line!");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
+    println!("{}", _info);
+
     loop {}
 }
