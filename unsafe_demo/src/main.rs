e0e66667cbc0b34f910778e08b47eb04711d63f8
wiji <wijigaming.yt@gmail.com>
2026-04-29 13:41:24 -0700

    snake game

--- unsafe_demo/src/main.rs
+++
@@ -1,9 +0,0 @@
-use unsafe_demo::split_at_mut;
-
-fn main() {
-    let mut array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-    let tuple = split_at_mut(array.as_mut(), 5);
-
-    println!("{:?}", tuple.0);
-    println!("{:?}", tuple.1);
-}
